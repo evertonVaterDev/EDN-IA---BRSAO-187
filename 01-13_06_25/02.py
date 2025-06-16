dea1e7490da288ff3d55df15470b35ecd331ea60
Everton Vater <matheusvatereverton@gmail.com>
2025-06-16 13:48:16 -0300

    resolução atividade 13.06.2025

---
+++ 01-13_06_25/02.py
@@ -0,0 +1,5 @@
+numero1 = int(input("Digite um numero inteiro: "))
+numero2 = int(input("Digite um numero inteiro: "))
+
+
+print("A soma dos dois numeros inteiros é ",numero1 + numero2)
---
+++ 01-13_06_25/04.py
@@ -0,0 +1,6 @@
+nome_do_produto = "Cadeira Infantil"
+preco_unitario =  12.40
+quantidade = 3
+
+
+print(f"a sua compra foi de {quantidade} und de {nome_do_produto} no valor de {preco_unitario} cada, o preço total foi de {quantidade * preco_unitario}")
